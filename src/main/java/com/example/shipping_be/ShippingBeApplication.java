76849d252a24b75009c73c62d7b2e4fea6745c83
Tarang Lakhmapurkar <tarangjlakh@gmail.com>
2024-04-27 18:05:08 +0530

    First commit

---
+++ src/main/java/com/example/shipping_be/ShippingBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shipping_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shipping_be/ShippingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shipping_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
